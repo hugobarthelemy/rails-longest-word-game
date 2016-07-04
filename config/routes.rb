c7daf68555a62b932b8a615fa17ace89e20d7a2d
hugo barthelemy <barthelemyhugo@gmail.com>
2016-07-04 17:14:16 +0200

    finit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/game'
+  get 'game' => 'pages#game'
+  get 'score' => 'pages#score'
+
+  get 'pages/score'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
